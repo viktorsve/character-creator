d6b0d19bc2a638e47f6ca548a97ec7826f00ac4b
Viktor Svensson <42525041+viktorsve@users.noreply.github.com>
2022-05-11 11:21:43 +0200

    Revert change

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
-      <CharacterEditor />
       <div className='PerspectiveBar'></div>
+      <CharacterEditor />
       <Footer />
     </>
   );
